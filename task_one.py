faca383eb7bfc4df501dd7cc3e4f08cf499cd4cf
Dovydas Menkevicius <d.menkevicius@gmail.com>
2023-10-26 14:21:31 +0300

    changed functions with print into function with returns

--- task_one.py
+++ task_one.py
@@ -35,16 +35,18 @@ class ParagraphHandler:
         return unique_words_count
 
     @staticmethod
-    def print_three_most_used_words(unique_words_count: Dict[str, int]) -> None:
+    def get_three_most_used_words(unique_words_count: Dict[str, int]) -> List[str]:
         """Function prints out the top 3 most used words. Provided dictionary format should be: {str : int}."""
+        most_used_words = []
         first_max_key = max(unique_words_count, key=unique_words_count.get)
-        print(first_max_key)
+        most_used_words.append(first_max_key)
         del unique_words_count[first_max_key]
         secound_max_key = max(unique_words_count, key=unique_words_count.get)
-        print(secound_max_key)
+        most_used_words.append(secound_max_key)
         del unique_words_count[secound_max_key]
         third_max_value = max(unique_words_count, key=unique_words_count.get)
-        print(third_max_value)
+        most_used_words.append(third_max_value)
+        return most_used_words
 
     @staticmethod
     def get_string_from_list(words_list: List[str]) -> str:
@@ -73,20 +75,21 @@ class ParagraphHandler:
         return sentences_information
 
     @staticmethod
-    def print_shortest_sentence_and_words_count(
+    def get_shortest_sentence_and_words_count(
         dict_information: Dict[str, Any]
-    ) -> None:
+    ) -> Dict[str, Any]:
         """Function prints out the shortest sentence and count of words in the shortest sentence."""
+        shortest_sentence_and_words_count = {}
         min_words_count: int = min(
             (count["lenght_in_words"] for count in dict_information.values())
         )
-        sentence = ""
+        shortest_sentence_and_words_count["lenght_in_words"] = min_words_count
         for value in dict_information.values():
             if value["lenght_in_words"] == min_words_count:
-                sentence = value["sentence"]
-        print(
-            f"\nThis is the shortest sentence:{sentence}. It has {min_words_count} words."
-        )
+                shortest_sentence_and_words_count["sentence"] = value["sentence"]
+        return shortest_sentence_and_words_count
+        
+
 
     @staticmethod
     def get_word_with_every_secound_letter_capitalized(word: str) -> str:
@@ -98,7 +101,7 @@ class ParagraphHandler:
         return "".join(letters_in_word)
 
     @staticmethod
-    def print_paragraph_with_every_secound_letter_captalized(paragraph: str) -> None:
+    def get_paragraph_with_every_secound_letter_captalized(paragraph: str) -> str:
         words_list = paragraph_handler.get_word_list_with_lowered_letters(paragraph)
         converted_paragraph = ""
         for word in words_list:
@@ -106,10 +109,10 @@ class ParagraphHandler:
                 paragraph_handler.get_word_with_every_secound_letter_capitalized(word)
                 + " "
             )
-        print(converted_paragraph)
+        return converted_paragraph
 
     @staticmethod
-    def print_reversed_paragraph(paragraph: str) -> None:
+    def get_reversed_paragraph(paragraph: str) -> str:
         sentences_list = paragraph_handler.get_list_of_sentences(paragraph)
         reversed_paragraph_list = []
         for sentence in sentences_list:
@@ -128,7 +131,7 @@ class ParagraphHandler:
                 else:
                     reversed_paragraph += word
         final_paragraph_version = reversed_paragraph.replace(" .", ". ")
-        print(final_paragraph_version)
+        return final_paragraph_version
 
 
 if __name__ == "__main__":
@@ -141,16 +144,19 @@ if __name__ == "__main__":
     )
     words_counter = paragraph_handler.get_unique_words_count(list_of_strings_in_lower)
     print(
-        f"Here are listed all words with count in paragraph:\n {words_counter}\n"
+        f"Here are listed all words with it count in paragraph:\n {words_counter}\n"
     )
     print("The top three most used words are:")
-    paragraph_handler.print_three_most_used_words(words_counter)
+    words_list = paragraph_handler.get_three_most_used_words(words_counter)
+    for word in words_list:
+        print(word)
     list_of_strings = paragraph_handler.get_list_of_sentences("The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is."
     )
     sentences_information = paragraph_handler.get_calculated_word_count_and_sentence(list_of_strings)
-    paragraph_handler.print_shortest_sentence_and_words_count(sentences_information)
+    shortest_sentence = paragraph_handler.get_shortest_sentence_and_words_count(sentences_information)
+    print(f"\nThe shortest sentence is:{shortest_sentence['sentence']}. It consist of {shortest_sentence['lenght_in_words']} words.")
     print("\nHere is the paragraph in which the first and every other letter from every word is capitalized:")
-    paragraph_handler.print_paragraph_with_every_secound_letter_captalized("The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is.")
+    print(paragraph_handler.get_paragraph_with_every_secound_letter_captalized("The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is."))
     print("\nHere is a version of the paragraph in which all words are in reversed order:")
-    paragraph_handler.print_reversed_paragraph("The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is.")
+    print(paragraph_handler.get_reversed_paragraph("The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is."))
     
---
+++ tests/task_one_tests.py
@@ -0,0 +1,8 @@
+import unittest
+
+from task_one import TextHandler
+
+class TestTextHandler(unittest.TestCase):
+    text_handler = TextHandler()
+
+    def 
